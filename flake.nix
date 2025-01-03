d27eb10ccf57928240641e0f4f50ab2f34d2839f
confusedalex <alex@confusedalex.dev>
2025-01-03 15:48:52 +0100

    refactor: restructure flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
     { nixpkgs, flake-utils, ... }:
@@ -17,9 +19,6 @@
             jdt-language-server
             maven
           ];
-          # shellHook = ''
-          #
-          # '';
         };
       }
     );
