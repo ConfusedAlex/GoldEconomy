859f8955c780119b27debf4453218d4345ac6b2c
confusedalex <alex@confusedalex.dev>
2025-01-07 08:28:16 +0100

    build: update to java 21

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            jdk17
+            jdk21
             jdt-language-server
             maven
             gradle
